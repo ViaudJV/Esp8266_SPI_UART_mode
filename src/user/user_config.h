64cf3d0d2b8103c37e642f25df69fb3871fce38d
Julien Viaud <julien.viaudfr@gmail.com>
2015-10-29 23:01:50 +0100

    -Modification du Makefile pour ajouter les librairies utile pour l'utilisation de l'uart -Premier Main avec connexion à un reseau (SSID et  PASSWORD à configurer dans user_config.h)
    TODO
      Ajouter des traces de connexion (osprintf est mis sur l'uart 1 et plus UART 0)
      Ajouter des fonctions de connexion pour tranferer des identifiant de connexion
      Ajouter un Serveur TCP et UDP
      Ajouter une réception de data via le SPI.
    
    Bonus
     OTA?

---
+++ src/user/user_config.h
@@ -0,0 +1,9 @@
+#ifndef USER_CONFIG_H
+#define USER_CONFIG_H
+
+#define HOSTNAME  "Chip_One_1"
+#define SSID  
+#define PASSWORD 
+
+
+#endif
---
+++ src/user/user_main.c
@@ -0,0 +1,86 @@
+#include "ets_sys.h"
+#include "osapi.h"
+#include "gpio.h"
+#include "os_type.h"
+#include "ip_addr.h"
+#include "espconn.h"
+#include "user_interface.h"
+#include "user_config.h"
+
+
+
+void uart0_rx_intr_handler(void *para)
+{
+	
+}
+
+void wifi_callback( System_Event_t *evt )
+{
+    os_printf( "%s: %d\n", __FUNCTION__, evt->event );
+    
+		char toto[7] = "hello\n\0";
+		uart0_tx_buffer(toto, 7);
+    switch ( evt->event )
+    {
+        case EVENT_STAMODE_CONNECTED:
+        {
+		
+		char toto[7] = "hello\n\0";
+		uart0_tx_buffer(toto, 7);
+	   	os_printf("connect to ssid %s, channel %d\n",
+			evt->event_info.connected.ssid,
+			evt->event_info.connected.channel);
+	    	break;
+        }
+
+        case EVENT_STAMODE_DISCONNECTED:
+        {
+	    os_printf("disconnect from ssid %s, reason %d\n",
+	                evt->event_info.disconnected.ssid,
+	                evt->event_info.disconnected.reason);
+	    
+	    deep_sleep_set_option( 0 );
+	    system_deep_sleep( 60 * 1000 * 1000 );  // 60 seconds
+
+	    break;
+        }
+
+        case EVENT_STAMODE_GOT_IP:
+        {
+            os_printf("ip:" IPSTR ",mask:" IPSTR ",gw:" IPSTR,
+                        IP2STR(&evt->event_info.got_ip.ip),
+                        IP2STR(&evt->event_info.got_ip.mask),
+                        IP2STR(&evt->event_info.got_ip.gw));
+            os_printf("\n");
+            break;
+        }
+        
+        default:
+        {
+            break;
+        }
+    }
+}
+
+
+void user_init( void )
+{
+    static struct station_config config;
+    uart_div_modify( 0, UART_CLK_FREQ / ( 115200 ) );
+    os_printf( "%s\n", __FUNCTION__ );
+    uart_init(115200,9600);
+    wifi_station_set_hostname( HOSTNAME );
+    wifi_set_opmode_current( STATION_MODE );
+    gpio_init();
+//os_printf sur Uart0
+	os_install_putc1((void *)uart0_write_char_no_wait);
+    uart0_tx_buffer("init", 4);
+    config.bssid_set = 0;
+    os_memcpy( &config.ssid, SSID, 32 );
+    os_memcpy( &config.password, PASSWORD, 64 );
+    wifi_station_set_config( &config );
+    wifi_station_connect();
+    wifi_set_event_handler_cb( wifi_callback );
+
+}
+

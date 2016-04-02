92976545a2173b4235af9afe24705b8d2b5d005a
Julien Viaud <julien.viaudfr@gmail.com>
2016-04-02 15:05:51 +0200

    -création d'unfichier test sous arduino

--- test/sketch_feb07b/sketch_feb07b.ino
+++ test/sketch_feb07b/sketch_feb07b.ino
@@ -1,9 +1,33 @@
-void setup() {
-  // put your setup code here, to run once:
-
+#include <SPI.h>
+int ss=10; // using digital pin 10 for SPI slave select
+int del=200; // used for various delays
+ 
+void setup()
+{
+  pinMode(ss, OUTPUT); // we use this for SS pin
+  SPI.begin(); // wake up the SPI bus.
+  SPI.setBitOrder(MSBFIRST);
+  // our MCP4162 requires data to be sent MSB (most significant byte) first
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void setValue(int value)
+{
+  digitalWrite(ss, LOW);
+  SPI.transfer(0); // send command byte
+  SPI.transfer(value); // send value (0~255)
+  digitalWrite(ss, HIGH);
+}
+ 
+void loop()
+{
+  for (int a=0; a<256; a++)
+  {
+    setValue(a);
+    delay(del);
+  }
+  for (int a=255; a>=0; --a)
+  {
+    setValue(a);
+    delay(del);
+  }
 }
